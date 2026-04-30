53d64902fc358f118aa71d135ec34c9b1d12adbf
faheeds <42218586+faheeds@users.noreply.github.com>
2026-04-30 12:59:32 -0700

    feat: hardcode Firebase client config values

--- lib/use-push-notifications.ts
+++ lib/use-push-notifications.ts
@@ -27,12 +27,12 @@ export function usePushNotifications() {
 
       if (!getApps().length) {
         initializeApp({
-          apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-          authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-          projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-          storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-          messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-          appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+          apiKey: "AIzaSyAHt7kSsNnR7dvlp87yzSzghocXEuFrZXg",
+          authDomain: "pscc-fd46b.firebaseapp.com",
+          projectId: "pscc-fd46b",
+          storageBucket: "pscc-fd46b.firebasestorage.app",
+          messagingSenderId: "539230006518",
+          appId: "1:539230006518:web:5c8d73cbebe3809dacd0d7",
         });
       }
 
